90eb2d67f48a06d2c9674bc9a0c8009870d70461
nnikkin <frolnik3@gmail.com>
2025-11-28 14:04:44 +0300

    first commit

---
+++ src/main/java/com/nikkin/devicesdb/Application.java
@@ -0,0 +1,17 @@
+package com.nikkin.devicesdb;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@PWA(name = "База данных запоминающих устройств", shortName = "MassStorageDevices")
+@Theme("my-theme")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/nikkin/devicesdb/MainView.java
@@ -0,0 +1,16 @@
+package com.nikkin.devicesdb;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+    }
+}
